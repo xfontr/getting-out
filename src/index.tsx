649527af6984c1d78fb733114011bae96a0be139
Xifre Font <xifre.font.97@gmail.com>
2022-09-29 16:35:55 +0200

    Created a global style component and installed manrope font

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GameContextProvider from "./Store/CallStatusContext/GameContextProvider";
+import "@fontsource/manrope/400.css";
+import "@fontsource/manrope/700.css";
+import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./styles/mainTheme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +14,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <GameContextProvider>
-      <App />
+      <ThemeProvider theme={mainTheme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
     </GameContextProvider>
   </React.StrictMode>
 );
