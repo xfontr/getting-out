4366a9438fd636c89aa64adf46a1851b1ec94695
Xifre Font <xifre.font.97@gmail.com>
2022-10-04 13:12:43 +0200

    Tested the get score ratio function

--- src/utils/getScoreRatio/getScoreRatio.ts
+++ src/utils/getScoreRatio/getScoreRatio.ts
@@ -1,6 +1,6 @@
 const maxTimeScore = 5000;
 
-const getScoreRatio = (totalTime: number): number =>
-  Math.floor(maxTimeScore / totalTime);
+const getScoreRatio = (totalTime: number, maxScore = maxTimeScore): number =>
+  Math.floor(maxScore / totalTime);
 
 export default getScoreRatio;
